30510d3fe14f2fc3768f2d8b3e081926703b6e41
Emily Yew <eyew@ucsd.edu>
2015-01-22 22:29:39 +0000

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -49,6 +49,7 @@ function projectClick(e) {
        $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
     } else {
        $(".project-description").fadeOut();
+       $(".project-description").remove();
     }
     
 
